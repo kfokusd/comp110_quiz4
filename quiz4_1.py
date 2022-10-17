aa66e3c6e46cc302fa2384e710d34499e942aa6f
kfokusd <93218564+kfokusd@users.noreply.github.com>
2022-10-16 19:46:01 -0700

    Add files via upload

---
+++ quiz4_1.py
@@ -0,0 +1,24 @@
+###########################
+## Write your code here
+###########################
+
+
+################################
+## Do NOT change anything below 
+################################
+def main():
+    simple_decode("quiz4_1_encoded.csv")
+
+if __name__ == "__main__":
+    main()
+
+
+# Expected Output
+#XComm 580
+#Qualcomm 18
+#Cisco 395
+#HP 28
+#IBM 2
+#Microsoft 3
+#Facebook 903
+#Amazon 12
